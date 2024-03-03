af70efa9998676595ea0788165bdcbfea3f3532a
Yezen Hijazin <yth77@tamu.edu>
2024-03-03 01:54:13 -0600

    working env, dropdown added, Starting UI added

---
+++ software/VibeWrist/Styles.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native';
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#ECEFF1', // Light blue background color
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 20,
+    },
+    illustrationContainer: {
+      marginBottom: 40,
+      // Add styling for illustration container if needed
+    },
+    startButton: {
+      backgroundColor: '#4CAF50', // Green button color
+      paddingHorizontal: 20,
+      paddingVertical: 10,
+      borderRadius: 8,
+    },
+    startButtonText: {
+      color: '#FFFFFF', // White button text color
+      fontSize: 18,
+      fontWeight: 'bold',
+    },
+  });
+  
