d86f7acdb64f8581819e2abfa375675893013d6b
Adam Vick <98659840+advicktory@users.noreply.github.com>
2024-04-05 13:03:20 -0500

    Update home.js
    Ozzie was working on this.

--- software/ReactNative/vibewrist/pages/homeScreenComponents/home.js
+++ software/ReactNative/vibewrist/pages/homeScreenComponents/home.js
@@ -5,47 +5,8 @@ import cycleLengthSelector from "./homeScreenCycleChooser.js";
 import CycleReport from "./homeScreenText.js";
 import StartButton from "./homeScreenStartButton"
 import ProgressBar from "./progressBar"
-import SavePreset from "./savePreset"
-// import _BackgroundTimer from "react-native-background-timer";
-
-// function onStartPress(isTimerOn) {
-//   // This is a function that will on completion will do the following
-//   // 0) Send the Bracelet: Which LEDs to light up, Break Length in Mins (Deep sleep timer), Vibration Strength (Dependant on RSSI Value), Vibration Selection (If applicable)
-//   // 1) Start: Vibration Pattern to the Bracelet
-//   // 2) During (Study Length): Send which LEDs, Vibration Strength. Also, violations will need to be recorded.
-//   // 3) End: Turn off all LEDs, Send 'End of Cycle' Pulse, Send Break Length (for deep sleep). Also, the study/break times will need to be recorded.
-//   if (isTimerOn) {
-//     _BackgroundTimer.runBackgroundTimer(() => {
-//       console.log("Howdy!");
-//     }, 2000);
-//   } else {
-//     _BackgroundTimer.stopBackgroundTimer();
-//   }
-// }
-
-function prepLedIllumniation() {
-  // To be completed when Anthony finishes the protocal.
-  // Study Time remaining will need to used here.
-}
-
-function prepBreakTimer() {
-  // To be completed when Anthony finishes the protocal.
-  //
-}
-
-function prepVibStrength() {
-  // To be completed when Anthony finishes the protocal.
-  // RSSI value will need to accessed here freqently.
-}
-
-function prepVibSelection() {
-  // To be completed when Anthony finishes the protocal.
-}
-
-function onViolation() {
-  // However we end up recording violations will need to be recorded here
-}
-
+import SavePreset from "./savePreset
+  
 export default function HomeScreen({ navigation }) {
   const [timerOn, setTimerOn] = useState(false);
 
