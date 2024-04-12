46a438ddf2f3aab80f7a2a49cb73ddc9bc4de5a2
unnamed <adam@ivick.us>
2024-04-11 20:14:30 -0500

    Stage one done.

--- software/ReactNative/vibewrist/App.js
+++ software/ReactNative/vibewrist/App.js
@@ -3,26 +3,28 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // User Shit
-// import { UserProvider } from "./pages/UserContext";
-// import User from "./pages/User";
+import { UserProvider } from "./pages/UserContext";
+import User from "./pages/User";
 
 //Screens
 import HomeScreen from "./pages/homeScreenComponents/home";
 import BleDeviceSettingsScreen from "./pages/bleScreenComponents/bleBraceletSettingsScreen";
 
 // These are meant to get the reference to the device to be accessed anywhere
-// const user = new User();
+const user = new User();
 
 function App() {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
+      <UserProvider user={user}>
         <Stack.Navigator initialRouteName="Home">
           <Stack.Screen name="Home" component={HomeScreen} />
           {/* <Stack.Screen name="cBle" component={BleConnectedScreen}>*/}
           {/*<Stack.Screen name="dBle" component={BleDisconnectedScreen} />*/}
           <Stack.Screen name="sBle" component={BleDeviceSettingsScreen} />
         </Stack.Navigator>
+      </UserProvider>
     </NavigationContainer>
   );
 }
--- software/ReactNative/vibewrist/pages/User.js
+++ software/ReactNative/vibewrist/pages/User.js
@@ -1,87 +1,87 @@
 // // Class used to quickly obtain user data.
-// export default class User {
-//   constructor(
-//     userName = 'unnamed',
-//     devName = 'VibeWrist', // Name of the bracelet
-//     serUUID = 'TTT', // Service UUID of the bracelet
-//     charUUID = 'SSS', // Characteristic UUID of the bracelet
-//     sLen = 0, // Users study length
-//     bLen = 0, // Users break length
-//     cAmo = 0, // Cycle Amount
-//     bRange = 0, // Buzz Range
-//     bDur = 0, // Buzz Duration
-//     bFreq = 0 // Buzz Frequency
-//   ) {
-//     this.userName = userName;
-//     this.devName = devName;
-//     this.serUUID = serUUID;
-//     this.charUUID = charUUID;
-//     this.sLen = sLen;
-//     this.bLen = bLen;
-//     this.cAmo = cAmo;
-//     this.bRange = bRange;
-//     this.bDur = bDur;
-//     this.bFreq = bFreq;
-//   }
-//   getUserName() {
-//     return this.userName;
-//   }
-//   getDeviceName() {
-//     return this.devName;
-//   }
-//   getSerUUID() {
-//     return this.serUUID;
-//   }
-//   getCharUUID() {
-//     return this.charUUID;
-//   }
-//   getStudyLength() {
-//     return this.sLen;
-//   }
-//   getBreakLength() {
-//     return this.bLen;
-//   }
-//   getCycleAmount() {
-//     return this.cAmo;
-//   }
-//   getBuzzRange() {
-//     return this.bRange;
-//   }
-//   getBuzzDuration() {
-//     return this.bDur;
-//   }
-//   getBuzzFrequency() {
-//     return this.bFreq;
-//   }
+export default class User {
+  constructor(
+    userName = "unnamed",
+    devName = "VibeWrist", // Name of the bracelet
+    serUUID = "TTT", // Service UUID of the bracelet
+    charUUID = "SSS", // Characteristic UUID of the bracelet
+    sLen = 0, // Users study length
+    bLen = 0, // Users break length
+    cAmo = 0, // Cycle Amount
+    bRange = 0, // Buzz Range
+    bDur = 0, // Buzz Duration
+    bFreq = 0, // Buzz Frequency
+  ) {
+    this.userName = userName;
+    this.devName = devName;
+    this.serUUID = serUUID;
+    this.charUUID = charUUID;
+    this.sLen = sLen;
+    this.bLen = bLen;
+    this.cAmo = cAmo;
+    this.bRange = bRange;
+    this.bDur = bDur;
+    this.bFreq = bFreq;
+  }
+  getUserName() {
+    return this.userName;
+  }
+  getDeviceName() {
+    return this.devName;
+  }
+  getSerUUID() {
+    return this.serUUID;
+  }
+  getCharUUID() {
+    return this.charUUID;
+  }
+  getStudyLength() {
+    return this.sLen;
+  }
+  getBreakLength() {
+    return this.bLen;
+  }
+  getCycleAmount() {
+    return this.cAmo;
+  }
+  getBuzzRange() {
+    return this.bRange;
+  }
+  getBuzzDuration() {
+    return this.bDur;
+  }
+  getBuzzFrequency() {
+    return this.bFreq;
+  }
 
-//   setUserName(newUserName) {
-//     this.userName = newUserName;
-//   }
-//   setDeviceName(newDevName) {
-//     this.devName = newDevName;
-//   }
-//   setSerUUID(newSerUUID) {
-//     this.serUUID = newSerUUID;
-//   }
-//   setCharUUID(newCharUUID) {
-//     this.charUUID = newCharUUID;
-//   }
-//   setStudyLength(newSLen) {
-//     this.sLen = newSLen;
-//   }
-//   setBreakLength(newBLen) {
-//     this.bLen = newBLen;
-//   }
-//   setCycleAmount(newCAmo) {
-//     this.cAmo = newCAmo;
-//   }
-//   setBuzzRange(newBRange) {
-//     this.bRange = newBRange;
-//   }
-//   setBuzzDuration(newBDur) {
-//     this.bDur = newBDur;
-//   }
-//   setBuzzFrequency(newBFreq) {
-//     this.bFreq = newBFreq;
-//   }
-// }
+  setUserName(newUserName) {
+    this.userName = newUserName;
+  }
+  setDeviceName(newDevName) {
+    this.devName = newDevName;
+  }
+  setSerUUID(newSerUUID) {
+    this.serUUID = newSerUUID;
+  }
+  setCharUUID(newCharUUID) {
+    this.charUUID = newCharUUID;
+  }
+  setStudyLength(newSLen) {
+    this.sLen = newSLen;
+  }
+  setBreakLength(newBLen) {
+    this.bLen = newBLen;
+  }
+  setCycleAmount(newCAmo) {
+    this.cAmo = newCAmo;
+  }
+  setBuzzRange(newBRange) {
+    this.bRange = newBRange;
+  }
+  setBuzzDuration(newBDur) {
+    this.bDur = newBDur;
+  }
+  setBuzzFrequency(newBFreq) {
+    this.bFreq = newBFreq;
+  }
+}
