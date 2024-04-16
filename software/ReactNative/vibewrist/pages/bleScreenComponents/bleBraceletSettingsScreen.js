2cc3fa029e74d066c6dbd7bc6a24591b079e266b
Ozzie Aguirre <ozzie.aguirre@tamu.edu>
2024-04-15 20:08:41 -0500

    fixed the user buzz preferences in settings, will update them now

--- software/ReactNative/vibewrist/pages/bleScreenComponents/bleBraceletSettingsScreen.js
+++ software/ReactNative/vibewrist/pages/bleScreenComponents/bleBraceletSettingsScreen.js
@@ -1,38 +1,111 @@
-import React, { useState } from "react";
-import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
-import { SelectList } from "react-native-dropdown-select-list";
-import braceletPng from "../../assets/blue_bracelet.png";
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import { SelectList } from 'react-native-dropdown-select-list';
+import braceletPng from '../../assets/blue_bracelet.png';
+import { useUser } from '../UserContext';
 
 export default function BleDeviceSettingsScreen({ navigation }) {
-  const [buzzSensitivity, setBuzzSensitivity] = useState("Default");
-  const [buzzRhythm, setBuzzRhythm] = useState("Short Quick Buzz");
-  const [buzzStrength, setBuzzStrength] = useState("Low");
+  const user = useUser();
 
-  const buzzSensitivityOptions = [
-    { key: 1, value: "Default" },
-    { key: 2, value: "Close" },
-    { key: 3, value: "Very Close" },
-  ];
+  const { buzzSensitivityDropdown, buzzSensitivityResponse } =
+    buzzSensitivitySelection(user.getBuzzRange());
+  const { buzzRhythmDropdown, buzzRhythmResponse } = buzzRhythmSelection(
+    user.getBuzzDuration()
+  );
+  const { buzzStrengthDropdown, buzzStrengthResponse } = buzzFrequencySelection(
+    user.getBuzzFrequency()
+  );
 
-  const buzzRhythmOptions = [
-    { key: "1", value: "Short Quick Buzz" },
-    { key: "2", value: "Three Quick Buzzes" },
-    { key: "3", value: "Aggie Special" },
-    { key: "4", value: "Five Short Pulses" },
-    { key: "5", value: "Three Long Pulses" },
-  ];
+  useEffect(() => {
+    user.setBuzzRange(buzzSensitivityResponse);
+    user.setBuzzDuration(buzzRhythmResponse);
+    user.setBuzzFrequency(buzzStrengthResponse);
+  }, [buzzSensitivityResponse, buzzRhythmResponse, buzzStrengthResponse]);
 
-  const buzzStrengthOptions = [
-    { key: "1", value: "Low" },
-    { key: "2", value: "Medium Low" },
-    { key: "3", value: "Medium High" },
-    { key: "4", value: "High" },
-  ];
+  function buzzSensitivitySelection(currentValue) {
+    const [buzzSensitivity, setBuzzSensitivity] = useState(currentValue);
+    const buzzSensitivityOptions = [
+      { key: 1, value: 'Default' },
+      { key: 2, value: 'Close' },
+      { key: 3, value: 'Very Close' },
+    ];
+
+    return {
+      buzzSensitivityDropdown: (
+        <SelectList
+          setSelected={setBuzzSensitivity}
+          data={buzzSensitivityOptions}
+          search={false}
+          placeholder={getValueByKey(currentValue, buzzSensitivityOptions)}
+          save="key"
+          inputStyles={styles.inputContainer}
+          dropdownTextStyles={styles.dropdownContainer}
+          boxStyles={styles.boxContainer}
+          dropdownStyles={styles.boxContainer}
+        />
+      ),
+      buzzSensitivityResponse: buzzSensitivity,
+    };
+  }
+
+  function buzzRhythmSelection(currentValue) {
+    const [buzzRhythm, setBuzzRhythm] = useState(currentValue);
+    const buzzRhythmOptions = [
+      { key: '1', value: 'Short Quick Buzz' },
+      { key: '2', value: 'Three Quick Buzzes' },
+      { key: '3', value: 'Aggie Special' },
+      { key: '4', value: 'Five Short Pulses' },
+      { key: '5', value: 'Three Long Pulses' },
+    ];
+
+    return {
+      buzzRhythmDropdown: (
+        <SelectList
+          setSelected={setBuzzRhythm}
+          data={buzzRhythmOptions}
+          search={false}
+          placeholder={getValueByKey(currentValue, buzzRhythmOptions)}
+          save="key"
+          inputStyles={styles.inputContainer}
+          dropdownTextStyles={styles.dropdownContainer}
+          boxStyles={styles.boxContainer}
+          dropdownStyles={styles.boxContainer}
+        />
+      ),
+      buzzRhythmResponse: buzzRhythm,
+    };
+  }
+
+  function buzzFrequencySelection(currentValue) {
+    const [buzzStrength, setBuzzStrength] = useState(currentValue);
+    const buzzStrengthOptions = [
+      { key: '1', value: 'Low' },
+      { key: '2', value: 'Medium Low' },
+      { key: '3', value: 'Medium High' },
+      { key: '4', value: 'High' },
+    ];
+
+    return {
+      buzzStrengthDropdown: (
+        <SelectList
+          setSelected={setBuzzStrength}
+          data={buzzStrengthOptions}
+          search={false}
+          placeholder={getValueByKey(currentValue, buzzStrengthOptions)}
+          save="key"
+          inputStyles={styles.inputContainer}
+          dropdownTextStyles={styles.dropdownContainer}
+          boxStyles={styles.boxContainer}
+          dropdownStyles={styles.boxContainer}
+        />
+      ),
+      buzzStrengthResponse: buzzStrength,
+    };
+  }
 
   const handleSave = () => {
-    // Handle saving here
-    navigation.navigate("Home", {
-      /* pass necessary data */
+    navigation.navigate('Home', {
+      user: user,
     });
   };
 
@@ -44,51 +117,15 @@ export default function BleDeviceSettingsScreen({ navigation }) {
       <View style={styles.settings}>
         <View style={styles.setting}>
           <Text style={styles.settingLabel}>Set range of detection:</Text>
-          <View style={styles.selectorContainer}>
-            <SelectList
-              setSelected={setBuzzSensitivity}
-              data={buzzSensitivityOptions}
-              search={false}
-              placeholder={buzzSensitivity}
-              save="value"
-              inputStyles={styles.inputContainer}
-              dropdownTextStyles={styles.dropdownContainer}
-              boxStyles={styles.boxContainer}
-              dropdownStyles={styles.boxContainer}
-            />
-          </View>
+          {buzzSensitivityDropdown}
         </View>
         <View style={styles.setting}>
           <Text style={styles.settingLabel}>Set vibration rhythm:</Text>
-          <View style={styles.selectorContainer}>
-            <SelectList
-              setSelected={setBuzzRhythm}
-              data={buzzRhythmOptions}
-              search={false}
-              placeholder={buzzRhythm}
-              save="value"
-              inputStyles={styles.inputContainer}
-              dropdownTextStyles={styles.dropdownContainer}
-              boxStyles={styles.boxContainer}
-              dropdownStyles={styles.boxContainer}
-            />
-          </View>
+          {buzzRhythmDropdown}
         </View>
         <View style={styles.setting}>
           <Text style={styles.settingLabel}>Set vibration strength:</Text>
-          <View style={styles.selectorContainer}>
-            <SelectList
-              setSelected={setBuzzStrength}
-              data={buzzStrengthOptions}
-              search={false}
-              placeholder={buzzStrength}
-              save="value"
-              inputStyles={styles.inputContainer}
-              dropdownTextStyles={styles.dropdownContainer}
-              boxStyles={styles.boxContainer}
-              dropdownStyles={styles.boxContainer}
-            />
-          </View>
+          {buzzStrengthDropdown}
         </View>
       </View>
       <TouchableOpacity style={styles.saveButton} onPress={handleSave}>
@@ -101,56 +138,56 @@ export default function BleDeviceSettingsScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#000000",
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#000000',
   },
   title: {
-    color: "white",
+    color: 'white',
     fontSize: 24,
     marginVertical: 10,
   },
   subtitle: {
-    color: "white",
+    color: 'white',
     fontSize: 18,
     marginBottom: 20,
   },
   setting: {
     marginBottom: 20,
-    alignItems: "center",
-    justifyContent: "center",
-    width: "100%",
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
   },
   settings: {
-    width: "100%",
+    width: '100%',
   },
   settingLabel: {
-    color: "white",
-    fontWeight: "bold",
+    color: 'white',
+    fontWeight: 'bold',
     marginBottom: 5,
   },
   inputContainer: {
-    color: "white",
-    fontWeight: "bold",
+    color: 'white',
+    fontWeight: 'bold',
   },
   dropdownContainer: {
-    color: "white",
-    fontWeight: "bold",
+    color: 'white',
+    fontWeight: 'bold',
   },
   boxContainer: {
-    borderColor: "white",
+    borderColor: 'white',
     margin: 5,
-    backgroundColor: "#1c1b1d",
+    backgroundColor: '#1c1b1d',
   },
   image: {
     width: 100,
     height: 100,
   },
   selectorContainer: {
-    width: "90%",
+    width: '90%',
   },
   saveButton: {
-    backgroundColor: "#3d85c6",
+    backgroundColor: '#3d85c6',
     // backgroundColor: "#0066ff",
     paddingVertical: 10,
     paddingHorizontal: 20,
@@ -158,9 +195,14 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
   saveButtonText: {
-    color: "white",
+    color: 'white',
     fontSize: 18,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginHorizontal: 40,
   },
 });
+
+function getValueByKey(key, options) {
+  const option = options.find((o) => o.key === key);
+  return option ? option.value : null;
+}
