d8b288751bc620399524a935b047a4640b0994b2
unnamed <adam@ivick.us>
2024-04-22 18:17:29 -0500

    The logo is now responds with the connection status of the device. Red is disconnected, blue is connected.

--- software/ReactNative/vibewrist/pages/bleScreenComponents/bleLEDfunct.js
+++ software/ReactNative/vibewrist/pages/bleScreenComponents/bleLEDfunct.js
@@ -1,15 +1,14 @@
-import { BleManager } from 'react-native-ble-plx';
-import { atob, btoa } from 'react-native-quick-base64';
-import getDistance from '../bleScreenComponents/bleDistance.js';
+import { BleManager } from "react-native-ble-plx";
+import { atob, btoa } from "react-native-quick-base64";
+import getDistance from "../bleScreenComponents/bleDistance.js";
 
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-async function manageStudyTime(dataCharacteristic, studyValue, breakValue) {
+async function manageStudyTime(dataCharacteristic, studyValue, breakvalue) {
   const oneThirdStudyTime = studyValue / 3;
   const msTomin = 60000;
-
 
   // Using an async function to await the completion of each timed section
   const executeAfterDelay = async (delay, callback) => {
@@ -18,38 +17,36 @@ async function manageStudyTime(dataCharacteristic, studyValue, breakValue) {
   };
 
   await executeAfterDelay(oneThirdStudyTime * msTomin, () => {
-    console.log('(1,1,1)');
-    const led1 = btoa('1,1,1');
+    console.log("(1,1,1)");
+    const led1 = btoa("1,1,1");
 
     dataCharacteristic.writeWithResponse(led1);
   });
 
-
   await executeAfterDelay(oneThirdStudyTime * msTomin, () => {
-    console.log('(1,2,1)');
-    const led2 = btoa('1,2,1');
+    console.log("(1,2,1)");
+    const led2 = btoa("1,2,1");
 
     dataCharacteristic.writeWithResponse(led2);
   });
 
   await executeAfterDelay(oneThirdStudyTime * msTomin, async () => {
-    dataCharacteristic.writeWithResponse(btoa('1,3,1'));
+    dataCharacteristic.writeWithResponse(btoa("1,3,1"));
     for (let i = 0; i < 4; i++) {
       await sleep(1000);
-      await dataCharacteristic.writeWithResponse(btoa('1,1,0'));
-      await dataCharacteristic.writeWithResponse(btoa('1,2,0'));
-      await dataCharacteristic.writeWithResponse(btoa('1,3,0'));
+      await dataCharacteristic.writeWithResponse(btoa("1,1,0"));
+      await dataCharacteristic.writeWithResponse(btoa("1,2,0"));
+      await dataCharacteristic.writeWithResponse(btoa("1,3,0"));
 
       await sleep(1000);
-      await dataCharacteristic.writeWithResponse(btoa('1,1,1'));
-      await dataCharacteristic.writeWithResponse(btoa('1,2,1'));
-      await dataCharacteristic.writeWithResponse(btoa('1,3,1'));
+      await dataCharacteristic.writeWithResponse(btoa("1,1,1"));
+      await dataCharacteristic.writeWithResponse(btoa("1,2,1"));
+      await dataCharacteristic.writeWithResponse(btoa("1,3,1"));
     }
-    await dataCharacteristic.writeWithResponse(btoa('1,1,0'));
-    await dataCharacteristic.writeWithResponse(btoa('1,2,0'));
-    await dataCharacteristic.writeWithResponse(btoa('1,3,0'));
+    await dataCharacteristic.writeWithResponse(btoa("1,1,0"));
+    await dataCharacteristic.writeWithResponse(btoa("1,2,0"));
+    await dataCharacteristic.writeWithResponse(btoa("1,3,0"));
     await sleep(3000);
   });
-
 }
 export default manageStudyTime;
