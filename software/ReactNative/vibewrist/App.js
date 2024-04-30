633e813a568069c5fe077f04b44613012d68780e
unnamed <adam@ivick.us>
2024-04-29 20:25:40 -0500

    Got all of the screen and the functions w/in them. Now working on the functions themselves.

--- software/ReactNative/vibewrist/App.js
+++ software/ReactNative/vibewrist/App.js
@@ -1,24 +1,36 @@
 //Imports
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // User Shit
-import { UserProvider } from './pages/UserContext';
-import User from './pages/User';
+import { UserProvider } from "./pages/UserContext";
+import User from "./pages/User";
 
 //Screens
 
 import HomeScreen from "./pages/homeScreenComponents/home";
 import BleDeviceSettingsScreen from "./pages/bleScreenComponents/bleBraceletSettingsScreen";
-import Login from "./pages/loginScreen/login.js"
-import Account from "./pages/accountScreen/account.js"
-import SignUp from "./pages/loginScreen/signup"
-import Leader from "./pages/leaderboardScreen/leaderboardScreen"
+import Login from "./pages/loginScreen/login.js";
+import Account from "./pages/accountScreen/account.js";
+import SignUp from "./pages/loginScreen/signup";
+import Leader from "./pages/leaderboardScreen/leaderboardScreen";
 
 // These are meant to get the reference to the device to be accessed anywhere
 const user = new User();
 
+/**
+ * App Module
+ * @module App Screen
+ * */
+
 function App() {
+  /**
+   * This function returns a NavigationContainer with all of the pages our application uses.
+   * @function App
+   * @param {None} - This function does not use any parameters.
+   * @returns {NavigationContainer}
+   * @inner
+   * */
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
@@ -30,9 +42,8 @@ function App() {
           <Stack.Screen name="Settings" component={BleDeviceSettingsScreen} />
           <Stack.Screen name="Login" component={Login} />
           <Stack.Screen name="Account" component={Account} />
-          <Stack.Screen name="Signup" component={SignUp}/>
-          <Stack.Screen name="Leader" component={Leader}/>
-
+          <Stack.Screen name="Signup" component={SignUp} />
+          <Stack.Screen name="Leader" component={Leader} />
         </Stack.Navigator>
       </UserProvider>
     </NavigationContainer>
